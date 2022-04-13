e9e6228dec9cf5020f1e9a3e094b50c4b9fef998
JEONGHO <gh7052@nate.com>
2022-04-13 18:03:09 +0900

    clean code + authButton click -> editText + button

--- app/src/main/java/com/example/carrotMarket/Login.java
+++ app/src/main/java/com/example/carrotMarket/Login.java
@@ -46,14 +46,20 @@ public class Login extends AppCompatActivity {
     InputMethodManager imm;
     Toolbar toolbar;
     Button authButton;
-    LinearLayout ll;
     TextView textView4;
     TextView textView5;
+    EditText editText2;
+    Button authOkButton;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.login);
+
+        textView4 = findViewById(R.id.textView4);
+        textView5 = findViewById(R.id.textView5);
+        editText2 = findViewById(R.id.editText2);
+        authOkButton = findViewById(R.id.button4);
 
         // 포커스
         phoneNumberEditText = findViewById(R.id.editText);
@@ -63,14 +69,13 @@ public class Login extends AppCompatActivity {
         imm = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
         imm.toggleSoftInput(InputMethodManager.SHOW_FORCED, InputMethodManager.HIDE_IMPLICIT_ONLY);
 
-        // 툴바 + 뒤로가기
+        // 툴바 + 뒤로가기 버튼 생성
         toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-        toolbar.setBackgroundColor(Color.rgb(255,255,255));
         getSupportActionBar().setTitle("");
-        getSupportActionBar().setDisplayHomeAsUpEnabled(true); // 뒤로가기 허용
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
-        // 인증번호 버튼 눌렀을때
+        // 인증번호 버튼 눌렀을때 이벤트 실행
         authButton = findViewById(R.id.button3);
         authButton.setOnClickListener(new View.OnClickListener() {
 
@@ -79,87 +84,37 @@ public class Login extends AppCompatActivity {
 
                 num = phoneNumberEditText.getText().toString().length();
 
+                // 10자리 이상 시
                 if(num >= 10) {
 
-                    ll = findViewById(R.id.ll);
-                    textView4 = findViewById(R.id.textView4);
-                    textView5 = findViewById(R.id.textView5);
-
-                    // 기존 View 애니메이션 이동
+                    // 기존 View 및 INVISIBLE View 애니메이션 이동
                     ObjectAnimator animation = ObjectAnimator.ofFloat(phoneNumberEditText,"translationY", -350f);
                     ObjectAnimator animation2 = ObjectAnimator.ofFloat(authButton,"translationY", -350f);
                     ObjectAnimator animation3 = ObjectAnimator.ofFloat(textView4,"translationY", -350f);
                     ObjectAnimator animation4 = ObjectAnimator.ofFloat(textView5,"translationY", -350f);
+                    ObjectAnimator animation5 = ObjectAnimator.ofFloat(editText2,"translationY", -350f);
+                    ObjectAnimator animation6 = ObjectAnimator.ofFloat(authOkButton,"translationY", -350f);
                     animation.setDuration(200);
                     animation2.setDuration(200);
                     animation3.setDuration(200);
                     animation4.setDuration(200);
+                    animation5.setDuration(200);
+                    animation6.setDuration(200);
                     animation.start();
                     animation2.start();
                     animation3.start();
                     animation4.start();
+                    animation5.start();
+                    animation6.start();
+                    editText2.setVisibility(View.VISIBLE);
+                    authOkButton.setVisibility(View.VISIBLE);
 
-                    // 동적 인증번호 입력 editText 생성
-                    EditText tempEtt = new EditText(getApplicationContext());
-                    tempEtt.setHint("인증번호 입력");
-                    tempEtt.setBackgroundResource(R.drawable.login_edittext_round);
-                    tempEtt.setTextSize(16);
-                    tempEtt.setLayoutParams(new LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT, 130));
-                    tempEtt.setY(-300);
-                    tempEtt.requestFocus();
-                    tempEtt.setPadding(30,0,0,0);
-                    tempEtt.setFilters(new InputFilter[] {new InputFilter.LengthFilter(11)});
-
-                    // 동적 주의 textView 생성
-                    TextView tempTvw = new TextView(getApplicationContext());
-                    tempTvw.setText("어떤 경우에도 타인에게 공유하지 마세요!");
-                    tempTvw.setTextSize(14);
-                    tempTvw.setLayoutParams(new LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT, 130));
-                    tempTvw.setY(-270);
-                    tempTvw.onEditorAction(3);
-
-                    // 동적 인증번호 확인 button 생성
-                    Button tempBtn = new Button(getApplicationContext());
-                    tempBtn.setText("인증번호 확인");
-                    tempBtn.setBackgroundResource(R.drawable.login_button_round_gray);
-                    tempBtn.setTextSize(18);
-                    tempBtn.setTypeface(Typeface.DEFAULT_BOLD);
-                    tempBtn.setTextColor(Color.parseColor("#FFFFFF"));
-                    tempBtn.setLayoutParams(new LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT, 130));
-                    tempBtn.setY(-300);
-
-                    // 리니어 레이아웃에 추가
-                    ll.addView(tempEtt);
-                    ll.addView(tempTvw);
-                    ll.addView(tempBtn);
+                    authButton.setText("인증문자 다시 받기");
                 }
             }
         });
 
-        /*
-        // 인증번호 확인 버튼
-        tempEtt.addTextChangedListener(new TextWatcher() {
-            @Override
-            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) { }
-
-            @Override
-            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
-                String text = charSequence.toString();
-                if(text.length() >= 6) {
-                    tempBtn.setBackgroundResource(R.drawable.login_button_round_orange);
-                } else {
-                    if(text.length() < 6) {
-                        tempBtn.setBackgroundResource(R.drawable.login_button_round_gray);
-                    }
-                }
-            }
-
-            @Override
-            public void afterTextChanged(Editable editable) {
-            }
-        })*/
-
-        // 버튼 개수 변화에 대한 authButton 색상 변화
+        // authButton 색상 변화
         phoneNumberEditText.addTextChangedListener(new TextWatcher() {
             @Override
             public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) { }
@@ -179,9 +134,30 @@ public class Login extends AppCompatActivity {
             @Override
             public void afterTextChanged(Editable editable) { }
         });
+
+        // authOkButton 색상 변화
+        editText2.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) { }
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+                String text = charSequence.toString();
+                if(text.length() == 6) {
+                    authOkButton.setBackgroundResource(R.drawable.login_button_round_orange);
+                } else {
+                    if(text.length() < 6) {
+                        authOkButton.setBackgroundResource(R.drawable.login_button_round_gray);
+                    }
+                }
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) { }
+        });
     }
 
-    // 뒤로가기 버튼 눌렀을때
+    // 뒤로가기 눌렀을때 이벤트
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         switch(item.getItemId()) {
